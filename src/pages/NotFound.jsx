0c03a016553c32ad5423121d26f5a5887bba9ed4
aires1294 <pedrotja.engenharia@gmail.com>
2022-09-07 20:15:59 -0300

    item 1 feitooo

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return <div data-testid="page-not-found">Nao achado</div>;
+  }
+}
+
+export default NotFound;
---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return <div data-testid="page-profile-edit">paginaaa</div>;
+  }
+}
+
+export default ProfileEdit;
