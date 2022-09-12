d30c800282eef9dd0329d13acd88cf580464dac8
aires1294 <pedrotja.engenharia@gmail.com>
2022-09-11 23:25:41 -0300

    item 5 feitooo

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,53 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      isDisabled: true,
+    };
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target;
+    this.setState({
+      name: value,
+    });
+    const USER_MIN_CHAR = 1;
+    const { name } = this.state;
+    // console.log(name);
+    if (name.length >= USER_MIN_CHAR) {
+      this.setState({
+        isDisabled: false,
+      });
+    } else {
+      this.setState({
+        isDisabled: true,
+      });
+    }
+  };
+
   render() {
+    const { name, isDisabled } = this.state;
     return (
-      <>
-        <div data-testid="page-search">Search</div>
+      <div>
         <Header />
-      </>
+        <div data-testid="page-search">Search</div>
+        <input 
+          data-testid="search-artist-input"
+          value={ name }
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          // onClick={  }
+          disabled={ isDisabled }
+        >
+          Pesquisar
+        </button>
+      </div>
     );
   }
 }
