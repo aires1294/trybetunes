5c62f0f3f817c5363c666daf8a153b6010a62563
aires1294 <pedrotja.engenharia@gmail.com>
2022-09-11 22:50:04 -0300

    item 4 feitooo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -22,6 +23,9 @@ class Header extends Component {
     return (
       <header data-testid="header-component">
         {loading ? <Loading /> : <p data-testid="header-user-name">{user}</p>}
+        <Link to="/search" data-testid="link-to-search"> Search </Link>
+        <Link to="/favorites" data-testid="link-to-favorites"> Favorites </Link>
+        <Link to="/profile" data-testid="link-to-profile"> Profile </Link>
       </header>
     );
   }
